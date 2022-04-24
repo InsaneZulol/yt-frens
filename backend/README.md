c873e88abfd48d419798b50bd8a9980ca8b0b59d
Karol Dzialowski <karlososhd@gmail.com>
2022-04-24 17:31:20 +0200

    add poc server with rooms with sandbox

--- backend/README.md
+++ backend/README.md
@@ -5,3 +5,5 @@ Backend server for playback synchronisation. POC of channels/rooms.
 ## Development
 
 Install packages with `npm install`. Start the server with `npm run start`.
+
+Open http://localhost:4000 to access the backend sandbox.
--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,68 @@ const http = require("http");
 const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const { instrument } = require("@socket.io/admin-ui");
+const io = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
 
+instrument(io, {
+  auth: false,
+});
+
+// http
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+// socket.io rooms
+const rooms = {};
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`)
+
+  socket.on("join room", async (roomId) => {
+    socket.roomId = roomId;
+    await socket.join(roomId);
+
+    if (rooms[roomId] === undefined) {
+      const room = {
+        clients: 1,
+      };
+      rooms[roomId] = room;
+      console.log(`user ${socket.id} created room ${roomId}`)
+
+      io.to(socket.roomId).emit('room stats', {usersCount: rooms[socket.roomId].clients})
+    } else {
+      rooms[roomId].clients++;
+      console.log(`user ${socket.id} joined room ${roomId}`)
+
+      io.to(socket.roomId).emit('room stats', {usersCount: rooms[socket.roomId].clients})
+    }
+  })
+
+  socket.on("play", () => {
+    console.log(`user ${socket.id} clicked play`);
+    socket.broadcast.to(socket.roomId).emit("play");
   });
 
-  socket.on('chat message', (msg) => {
-    socket.broadcast.emit('chat message', msg); // emits to everyone else
+  socket.on("pause", () => {
+    console.log(`user ${socket.id} clicked pause`);
+    socket.broadcast.to(socket.roomId).emit("pause");
+  });
+
+  socket.on("change time", (time) => {
+    console.log(`user ${socket.id} changed time to ${time}`);
+    socket.broadcast.to(socket.roomId).emit("change time", time);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} disconnected`)
+    // TODO: disconnecting from rooms and removing existing rooms
+    io.to(socket.roomId).emit('room stats', {usersCount: rooms[socket.roomId].clients})
   });
 });
 
