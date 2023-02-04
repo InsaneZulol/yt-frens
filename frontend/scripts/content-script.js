7e9ac150eea9b2e55075050f6365e34f8d7d84c3
Alan Kaluza <senomz@gmail.com>
2023-02-04 01:24:53 +0100

    dbg panel adjustments

--- frontend/scripts/content-script.js
+++ frontend/scripts/content-script.js
@@ -1,15 +1,22 @@
-function fun() {
-    console.log('xxx');
-    var _info = document.getElementById('info-contents').querySelector("#info");
-    _info.insertAdjacentHTML('beforebegin', "<div id='debug_panel'>xxx</div>");
+function insertDebugPanel() {
+    console.log('xx');
+    let info_ = document.querySelector('#above-the-fold');
+    // https://developer.mozilla.org/pl/docs/Web/API/Element/insertAdjacentHTML
+    info_.insertAdjacentHTML('afterbegin', `
+        <div class='debug_panel'>
+            <p>DEBUG PANEL KURWA TEN<p><br>
+            <p>Your room id is <p> <p class="dbg_room_id_nr"></p>
+            <h2>Join</h2>
+            <input type="text" id="roomId" name="room" minlength="1" maxlength="4" size="4">
+            <button class="dbg-join_btn" type="button" onclick="join_room()">Join</button> <br>
+            <h2>Create</h2>
+            <button class="dbg-create_btn" type="button" onclick="create_room()">Create</button>
+        </div>
+        `);
 }
 
 var CONTENTSCRIPT = (function () {
-    // var _video = document.querySelector('video');
-    // var _info = document.getElementById('info-contents').querySelector("#info");
-    console.log('content script injeeeected');
-    
-    // https://developer.mozilla.org/pl/docs/Web/API/Element/insertAdjacentHTML
+    console.log('content script injected');
+    setTimeout(insertDebugPanel, 800); // ładowanie skryptu na document_idle nie wystarcza
     // todo: change it to async await till this dom object exists or something
-    setTimeout(fun, 1500);
 })();
